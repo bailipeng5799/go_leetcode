6881042a538f9b57fe6c6fa13532e2549fd381c5
White <2648204533@qq.com>
2021-10-29 17:41:41 +0800

    堆排topk

--- NC/NC119.go
+++ NC/NC119.go
@@ -1,32 +1,38 @@
 package main
 
-func HeapSortMin(input []int,length int)[]int{
-	if length <= 1{
-		return input
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * @param input int整型一维数组
+ * @param k int整型
+ * @return int整型一维数组
+ */
+
+func HeapSortMin(input []int,length int){
+	if len(input) <= 1 {
+		return
 	}
-	depth := length / 2 -1
-	for i := depth;i >= 0;i--{
-		topmin := i // 最小的位置就是i
-		leftchild := 2 * i + 1//这个非叶子结点的左右子树下标
-		rightchild :=2 * i + 2
-		if input[leftchild] < input[topmin] && leftchild <= length -1{
+	depth := length/2 - 1
+	for i := depth; i >= 0; i-- {
+		topmin := i
+		leftchild := i*2 + 1
+		rightchild := i*2 + 2
+		if leftchild <= length-1 && input[leftchild] < input[topmin] {
 			topmin = leftchild
 		}
-		if input[rightchild] < input[topmin] && rightchild <= length -1{
+		if rightchild <= length-1 && input[rightchild] < input[topmin] {
 			topmin = rightchild
 		}
-		if topmin != i{
-			input[topmin],input[i]=input[i],input[topmin]
+		if topmin != i {
+			input[topmin],input[i] = input[i],input[topmin]
 		}
 	}
-	return input
 }
 func GetLeastNumbers_Solution( input []int ,  k int ) []int {
-	for i := 0;i < k;i++{
+	for i := 0; i < k; i++ {
 		lastlen := len(input) - i
-		HeapSortMin(input,lastlen)
+		HeapSortMin(input,lastlen)//将最小的元素放到了 0 这个位置上
 		if i < len(input) {
-			//将最小元素沉到数组末端
 			input[0],input[lastlen-1] = input[lastlen-1],input[0]
 		}
 	}
