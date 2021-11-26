3f579014b861f302fb80f9a32a27728d52692a31
White <2648204533@qq.com>
2021-11-26 18:45:02 +0800

    二叉树的镜像

---
+++ offer2/27.go
@@ -0,0 +1,11 @@
+package main
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	//因为root.Left会被置换
+	tmp := root.Left
+	root.Left = mirrorTree(root.Right)
+	root.Right = mirrorTree(tmp)
+	return root
+}
